8e172d454e0bcf029e2fac75c5e4028c94a98092
lskatz <lskatz@347234af-12d4-46d6-bdbb-00f489ae4c7f>
2011-03-10 20:11:47 +0000

    corrected a mistake in UniProt BLAST (BLAST+ part of it)

--- cg_pipeline/trunk/scripts/run_annotation_blast.pl
+++ cg_pipeline/trunk/scripts/run_annotation_blast.pl
@@ -32,10 +32,16 @@ die("File $$settings{query_mfa} does not exist") unless -f $$settings{query_mfa}
 $$settings{tempdir} ||= tempdir(File::Spec->tmpdir()."/$0.$$.XXXXX", CLEANUP => !($$settings{keep}));
 logmsg "Temporary directory is $$settings{tempdir}";
 
+whichBlast($settings); # which blast? blast+?  Modify the settings.
+$$settings{num_cpus}=AKUtils::getNumCPUs();
+$$settings{blast_max_hits} ||= 250;
+
+# TODO speed up blast by limiting the number of results
 my $bf = Bio::Tools::Run::StandAloneBlast->new(database => $$settings{blast_db},
 											   outfile => "$$settings{tempdir}/$0.$$.blast_out",
 											   program => 'blastp',
-											   a => AKUtils::getNumCPUs());
+											   a => $$settings{num_cpus});
+
 
 my $inseq = Bio::SeqIO->new(-file => "<$$settings{query_mfa}", -format => 'fasta');
 
@@ -48,7 +54,21 @@ open(BLAST_OUT_SQL, '>', $$settings{blast_sql_file}) or die "Unable to open file
 
 my $i;
 while (my $seq = $inseq->next_seq) {
-	my $report = $bf->blastall($seq);
+  my $report;
+  
+  # perform the query
+  if($$settings{blast_version} eq 'blast+'){
+    my $seqString=">".$seq->id."\n".$seq->seq;
+    my $blastCommand="echo '$seqString'|blastp -query -db $$settings{blast_db} -num_threads $$settings{num_cpus} -out $$settings{tempdir}/$0.$$.blast_out ";
+    #$blastCommand.="-num_descriptions $$settings{blast_max_hits} -num_alignments $$settings{blast_max_hits} ";
+    system($blastCommand); die if $?;
+    #system("echo $$settings{tempdir}/$0.$$.blast_out");exit;
+    $report=new Bio::SearchIO(-format=>'blast',-file=>"$$settings{tempdir}/$0.$$.blast_out");
+  }
+  else{
+  	$report = $bf->blastall($seq);
+  }
+
 	my $h;
 	while (my $result = $report->next_result) {
 		$h++; last if $h > 15;
@@ -72,3 +92,19 @@ logmsg "Processed $i proteins, done";
 close BLAST_OUT_SQL;
 
 logmsg "Printed $$stats{hit_record_count} hits to $$settings{blast_sql_file}";
+
+# modify settings to figure out which version of blast we are dealing with
+# TODO if need be, put this into AKUtils
+# TODO allow for whichBlast to pick up on the other four flavors of blast
+sub whichBlast{
+  my($settings)=@_;
+  my $version="before2009";
+  $$settings{path_to_blast}=AKUtils::fullPathToExec("blastall")||AKUtils::fullPathToExec("blastp")||"";
+  if($$settings{path_to_blast}=~/blastall/){
+    $$settings{blast_version}="before2009";
+  }
+  elsif($$settings{path_to_blast}=~/blastp/){
+    $$settings{blast_version}="blast+";
+  }
+}
+
