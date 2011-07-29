a53c283614e923ce921ff0ac983449eb2bcb2d33
lskatz <lskatz@347234af-12d4-46d6-bdbb-00f489ae4c7f>
2011-07-29 18:43:39 +0000

    More updates to the NCBI submission creator

--- cg_pipeline/branches/lkatz/scripts/run_pipeline_NCBI.pl
+++ cg_pipeline/branches/lkatz/scripts/run_pipeline_NCBI.pl
@@ -26,7 +26,7 @@ use File::Basename;
 use List::Util qw(min max sum shuffle);
 use CGPipelineUtils;
 use Data::Dumper;
-use Switch 'fallthrough';
+use Bio::Perl;
 
 my $verbose;
 
@@ -38,27 +38,32 @@ exit(main());
 sub main(){
   $settings = AKUtils::loadConfig($settings);
   die usage() if(@ARGV<1);
-  GetOptions($settings,qw(help! project=s tempdir=s keep!));
+  GetOptions($settings,qw(help! genbank=s tempdir=s keep!));
   die usage() if($$settings{help});
   $$settings{tempdir} ||= tempdir(File::Spec->tmpdir()."/$0.$$.XXXXX", CLEANUP => !($$settings{keep}));
 
   # check for required files
-  my @file=qw(assembly.fasta annotation.gb template.sbt);
-  for(my $i=0;$i<@file;$i++){
-    $file[$i]=File::Spec->rel2abs("$$settings{project}/$file[$i]");
-    if(-s $file[$i] < 1){
-      die "File does not exist or is 0 bytes: $file[$i]";
-    }
-  }
-  my($assembly,$annotation,$template)=@file;
-
-  my $template=createTemplate($settings);
+  my $genbank=$$settings{genbank} or die "Error: missing genbank parameter\n".usage();
+  $genbank=File::Spec->rel2abs($genbank);
 
   # add on anything necessary for the defline, etc
-  $assembly=defineAssembly($assembly,$settings);
+  my $assembly=defineAssembly($genbank,$settings);
   logmsg "Tagged assembly file has been printed to $assembly";
 
+  # Need to figure out how to create ASN file
+  # http://biostar.stackexchange.com/questions/10748/read-writer-for-asn
+  #my $template=createTemplate($settings);
+
+  my $tbl=createTbl($genbank,$settings);
+
   return 0;
+}
+
+
+# See: http://www.ncbi.nlm.nih.gov/Sequin/table.html#Table Layout
+sub createTbl{
+  my($genbank,$settings)=@_;
+  die "TODO: this subroutine";
 }
 
 # Creates an NCBI template file http://www.ncbi.nlm.nih.gov/genbank/tbl2asn2.html#Template
@@ -68,46 +73,64 @@ sub main(){
 # TODO put in any necessary config options such as pipeline_authors_address
 sub createTemplate{
   my($settings)=@_;
-  
+
+  # make array of authors
+  my @author;
+  my $a=1;
+  while($$settings{"author$a"}){
+    my %a;
+    ($a{name},$a{affiliation},$a{div},$a{city},$a{country},$a{street},$a{email},$a{phone},$a{zipCode})=split(/\|/,$$settings{"author$a"});
+    push(@author,\%a);
+    $a++;
+  };
+  print Dumper \@author;exit;
+
+  # make corresponding authors
+
+  # make comprehensive author list
 }
+
 
 # Adds on additional tags to the assembly.
 # Assumes a unique seqid is already present.
 # Does not change the actual assembly data.
 # TODO just grab the assembly from the genbank file to reduce the number of necessary files, to keep consistency, and to promote modularity/portability of CG-Pipeline
 sub defineAssembly{
-  my($assembly,$settings)=@_;
+  my($genbank,$settings)=@_;
   my $outfile="$$settings{tempdir}/assembly.fasta";
-  my @classification=split(/\s+/,$$settings{classification});
-  my $scientificName=join(" ",@classification[0..1]);
-  my $strain=$$settings{project};
-  my $seqs=AKUtils::readMfa($assembly,$settings);
-  my $newSeqs;
-  while(my($seqname,$seq)=each(%$seqs)){
+  my $seqin=Bio::SeqIO->new(-file=>$genbank,-format=>"genbank");
+  my $seqout=Bio::SeqIO->new(-file=>">$outfile");
+
+  while(my $seq=$seqin->next_seq){
+    #TODO use Bio::Taxon instead because Bio::Species is becoming deprecated
+
+    #TODO there is not a reliable way of getting the genome name yet
+    # Get the strain name from the locus name, e.g. 2010V-1031_0001 => 2010V-1031
+    my $scientificName=$seq->species->binomial;
+    my @f=split(/_/,$seq->id);
+    my $seqid=pop(@f);
+    my $strain=join("_",@f);
+
+    $seq->desc("[organism=$scientificName] [strain=$strain]");
+    $seq->id($seqid);
+
+    $seqout->write_seq($seq);
+
+    # TODO get classification and scientific name from the genbank file
     # TODO define any other tags here.
     # Allowable tags: strain, isolate, chromosome, topology, locatino, molecule, technique, protein, genetic code
-    if($seqname!~/organism=/){
-      $seqname.=" [organism=$scientificName]";
-    }
-    if($seqname!~/strain=/){
-      $seqname.=" [strain=$strain]";
-    }
-    $$newSeqs{$seqname}=$seq;
+    #my @feat=$seq->get_SeqFeatures;
   }
 
   # make a new assembly file and return its new name
-  AKUtils::printSeqsToFile($newSeqs,$outfile,$settings);
   return $outfile;
 }
 
 sub usage{
   "Turns your genome project directory into something that you can submit to NCBI
-  Usage: perl $0 -p project
-  Where the project directory has the following files:
-    assembly.fasta
-    annotation.gb
+  Usage: perl $0 -g genbank.gbk
   Arguments
-    -p project directory name
+    -g genbank annotated file
     -h (no arguments) this help menu
     -k (no arguments) to keep temporary directory around. 
       It will be kept without specifying if the temp dir name is given.
