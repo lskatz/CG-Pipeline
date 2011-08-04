354381979bfad0c7d390c8742c7f4769858d96c2
jhumphrey6 <jhumphrey6@347234af-12d4-46d6-bdbb-00f489ae4c7f>
2011-08-04 14:47:55 +0000

    a tool that uses the new Store module

--- cg_pipeline/branches/jhumphrey6/trunk/scripts/cg_gff3_to_genbank.pl
+++ cg_pipeline/branches/jhumphrey6/trunk/scripts/cg_gff3_to_genbank.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# author: Jay Humphrey <jhumphrey6@gatech.edu>
+# author: Jay Humphrey <jhumphrey6@gmail.com>
 # convert gff to genbank
 
 use warnings;
@@ -7,15 +7,15 @@ use strict;
 use File::Path;
 use FindBin;
 use lib "$FindBin::RealBin/../lib";
-use CGPipeline::SQLiteDB; 
+use CGPipeline::Store; 
 $0 =~ s/^.*\/([^\/]+)$/$1/;
 if(!@ARGV){print "usage: $0 input_fasta_file input_gff_file output_genbank_filename\n";exit;}
 my ($fnain,$gffin,$gbout)=@ARGV;
 my $sqlitedb=sprintf("%s.%d.sqlite",$gbout,time);
 print "Creating $sqlitedb...\n";
-my $cgp = CGPipeline::SQLiteDB->new(file=>$sqlitedb);
+my $cgp = CGPipeline::Store->new(file=>$sqlitedb);
 $cgp->load_fasta($fnain);
-$cgp->load_gff($gffin);
+#$cgp->load_gff($gffin);
 $cgp->write_genbank($gbout);
 print "Removing $sqlitedb...\n";
 system("rm $sqlitedb");
