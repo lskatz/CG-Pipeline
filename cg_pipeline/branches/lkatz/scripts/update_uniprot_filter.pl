c1d834cc4854e74ad83c7e1078a2118a5b54b2f6
Lee Katz <lskatz@users.noreply.github.com>
2011-11-21 04:43:51 +0000

    Trying out a filtering script to see if formatting and using the uniprot database can take less time

--- cg_pipeline/branches/lkatz/scripts/update_uniprot_filter.pl
+++ cg_pipeline/branches/lkatz/scripts/update_uniprot_filter.pl
@@ -4,9 +4,9 @@
 # The mechanism is there but the ID doesn't match up with locus name...?
 
 # Filter out genes from a fasta file that belong to Eukaryotes
-# Or, retain genes that belong to prokies
 # author: Lee Katz <lkatz@cdc.gov>
 
+package PipelineRunner;
 use strict;
 use warnings;
 use Getopt::Long;
@@ -24,6 +24,7 @@ exit(main());
 
 sub main{
   my $settings={
+    appname=>"cgpipeline",
     url=>"ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/docs",
     filterOut=>[qw(yeast human humchr rice arath calbican cdlist celegans dicty fly hoxlist mgdtosp pkinfam pombe scorpktx)],
   };
@@ -34,35 +35,24 @@ sub main{
   my $outfile=$$settings{outfile} or die "Error: no outfile given\n".usage($settings);
   my $url=$$settings{url} or die "Internal error with URL parameter";
   my @filterOut=@{ $$settings{filterOut} };
-  my %filterOutIndex;
-    $filterOutIndex{$_}=1 for(@filterOut);
 
   # read which proteins to filter out
   my @proteinList=getBlacklist($url,\@filterOut,$settings);
 
-  # post-process
-  for (@proteinList){
-    s/^\s+|\s+$//g;
-    $_="" if(/\s+/);
-    next if(!/\(|;/);
-    # ($locus)=>$locus
-    s/^\(|\)$//g;
-    # $locus; => $locus
-    s/;$//;
-  }
-  
   my %proteinList;
   $proteinList{$_}=1 for (@proteinList);
   logmsg "Done with filtering and indexing proteins to filter out";
 
   # read the infile(s) and write the filtered entries to the outfile
   logmsg "Writing the output file";
+  my $seqCount=0;
   my $out=Bio::SeqIO->new(-file=>">$outfile");
   for my $file(@$infile){
     my $in=Bio::SeqIO->new(-file=>$file);
     while(my $seq=$in->next_seq){
       next if($proteinList{$seq->id});
       $out->write_seq($seq);
+      logmsg "Finished with $seqCount sequences" if(++$seqCount%1000000==0);
     }
   }
 
@@ -72,8 +62,22 @@ sub main{
 
 sub getBlacklist{
   my($url,$filterOut,$settings)=@_;
-  my $urlContent=get($url);
   my (@blacklist);
+  my $tmpfile="$$settings{tempdir}/blacklist.txt";
+  
+  # if the blacklist already exists, just read the file to save time
+  if(-e $tmpfile){
+    logmsg "Blacklist already exists at $tmpfile; reading instead of recreating it";
+    open(FILE,$tmpfile) or die "Could not open $tmpfile because $!";
+    while(<FILE>){
+      chomp;
+      push(@blacklist,$_);
+    }
+    close FILE;
+    return @blacklist;
+  }
+
+  my $urlContent=get($url);
   for my $fileInfo(split(/\n/,$urlContent)){
     my $file=(split(/\s+/,$fileInfo))[-1];
     my($search,$ext)=fileparse($file,qw(.txt));
@@ -81,28 +85,47 @@ sub getBlacklist{
     my $is_blacklisted=scalar(grep(/$search/i,@$filterOut));
     next if(!$is_blacklisted);
 
-    #next if($search ne 'yeast'); # DEBUG
-
     my @blacklistGenes=genesListedInUniptrotTxt($file,$url,$settings);
-    die "Did not blacklist any genes from $file" if (!@blacklistGenes);
+    warn "Did not blacklist any genes from $file" if (!@blacklistGenes);
     logmsg "$file yielded ".scalar(@blacklistGenes)." genes";
     push(@blacklist,@blacklistGenes);
   }
+  # post-process
+  for (@blacklist){
+    s/^\s+|\s+$//g;
+    $_="" if(/\s+/);
+    next if(!/\(|;/);
+    # ($locus)=>$locus
+    s/^\(|\)$//g;
+    # $locus; => $locus
+    s/;$//;
+  }
+  @blacklist=grep(!/^\s*$/,@blacklist);
+
+  # write the blacklist to file to cache it
+  # /(sp|tr)\|[\dA-Z]{6}/
+  open(FILE,">",$tmpfile) or die "Could not open $tmpfile for writing because $!";
+  print FILE "$_\nsp|$_\ntr|$_\n" for(@blacklist);
+  close FILE;
+  logmsg "Cached the blacklist at $tmpfile";
+  
   return @blacklist;
 }
 
-# parse a text file on uniprot to find which genes are listed
+# download and parse a text file on uniprot to find which genes are listed
 sub genesListedInUniptrotTxt{
   my($file,$url,$settings)=@_;
   
   $file="$url/$file";
   my @locus;
-  my $seen_header=0;
   for my $line(split(/\n/,get($file))){
     # test the first few fields
-    for my $locus((split(/\s{2,}/,$line))[0..6]){
+    for my $locus(split(/\s{2,}/,$line)){
       if(is_locus($locus)){
-        push(@locus,split(/\s*;\s*/,$locus));
+        my @theseLoci=split(/\s*;\s*/,$locus);
+        for my $l(@theseLoci){
+          push(@locus,$l) if(is_locus($l));
+        }
       }
     }
   }
@@ -110,18 +133,12 @@ sub genesListedInUniptrotTxt{
 }
 
 # see if a locus matches some patterns
+# Every locus is a 6-digit thing
+#   grep ">" uniprot_sprot_trembl.fasta|perl -lane 'die "$_" if(!/(sp|tr)\|[\dA-Z]{6}/)'
 sub is_locus{
   my($locus)=@_;
-
   return 0 if(!defined($locus));
-  return 0 if($locus=~/^\s*$/);
-  # a locus does not start with a dash or underscore
-  return 0 if($locus=~/^\s*[\-_]/);
-  # a locus does not have lowercase, but if it does then it will have a digit
-  # but limit the string length because there might be a huge chemical name
-  #     e.g. 6-methopentyl.....5-N-kinase
-  return 0 if($locus=~/[a-z]/ && $locus!~/.+\d/ && length($locus) > 8);
-
+  return 0 if($locus!~/[0-9A-Z]{6}/);
   return 1;
 }
 
@@ -137,6 +154,8 @@ sub usage{
   -u URL
     The basename URL for where genes are listed.
     Defaut: $$settings{url}
+  -t directory
+    a temporary directory. Default: a newly created dir under /tmp/
   -h
     This help menu
   ";
