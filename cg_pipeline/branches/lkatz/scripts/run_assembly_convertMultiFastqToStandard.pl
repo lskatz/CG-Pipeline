3376263b7965b00ea002b7c0bf89e3c448f197bd
Lee Katz <lskatz@users.noreply.github.com>
2012-05-03 22:14:55 +0000

    After finding out that nonstandard fastq files exist, I wrote a converter

---
+++ cg_pipeline/branches/lkatz/scripts/run_assembly_convertMultiFastqToStandard.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+
+# Some fastq files are nonstandard, and each entry spans more than four rows, making them difficult to parse.
+# This script converts those entries.
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Getopt::Long;
+
+exit(main());
+
+sub main{
+  my $settings={};
+  GetOptions($settings,qw(infile=s outfile=s minLength=i));
+  my $in=$$settings{infile} or die usage();
+  my $out=$$settings{outfile} or die usage();
+
+  convert($in,$out,$settings);
+}
+
+sub convert{
+  my($in,$out,$settings)=@_;
+  open(IN,$in) or die "Could not open $in: $!";
+  open(OUT,">$out") or die "Could not open $out for writing: $!";
+  while(my $id=<IN>){
+    my($qual,$sequence);
+    chomp($id);
+    $id=~s/^@//;
+    while(my $tmp=<IN>){
+      last if($tmp=~/^\+(\Q$id\E)?/);
+      $sequence.=$tmp;
+    }
+    my $seqLength=length($sequence);
+
+    for(1..$seqLength){
+      $qual.=getc(IN);
+    }
+    $qual=~s/\s+//g;
+    $sequence=~s/\s+//g;
+
+    next if($$settings{minLength} && length($sequence)<$$settings{minLength});
+
+    print OUT "\@$id\n$sequence\n+\n$qual\n";
+  }
+  close OUT;
+  close IN;
+}
+
+sub usage{
+  "Converts a multi-line fastq file to the standard 4-lines per entry, standard form
+  Usage: $0 -i nonstandard.fastq -o standard.fastq [other options]
+  -m minimum length for a sequence to be outputted, in base pairs
+  ";
+}
