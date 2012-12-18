e65968800dd65ba59c04d7eea34abe154c4116f8
Lee Katz <lskatz@users.noreply.github.com>
2012-12-18 20:10:03 +0000

    updated the interpro script with a progress subroutine. Added in error checking for databases in run_annotation, with instructions to add the correct database, if there is an error

--- cg_pipeline/branches/lkatz/scripts/run_annotation_interpro.pl
+++ cg_pipeline/branches/lkatz/scripts/run_annotation_interpro.pl
@@ -18,25 +18,54 @@ use File::Basename;
 use Bio::SeqIO;
 use XML::LibXML::Reader;
 
+use threads;
+use Thread::Queue;
+
 $0 = fileparse($0);
 
-my $usage = "$0 [-outfile=parsed_interpro_output.sql] input.mfa";
+exit(main());
 
-my @cmd_options = ('tempdir=s', 'outfile=s', 'keep');
-GetOptions($settings, @cmd_options) or die "Usage: $usage";
-die("Usage: $usage") if @ARGV != 1;
-#die("Argument blast_db must be supplied") unless $$settings{blast_db};
-$$settings{query_mfa} = $ARGV[0];
-die("File $$settings{query_mfa} does not exist") unless -f $$settings{query_mfa};
-$$settings{outfile} ||= "$$settings{query_mfa}.iprscan_out.xml";
+sub main{
+  my $usage = "$0 [-outfile=parsed_interpro_output.sql] input.mfa";
+  my @cmd_options = ('tempdir=s', 'outfile=s', 'keep');
+  GetOptions($settings, @cmd_options) or die "Usage: $usage";
+  die("Usage: $usage") if @ARGV != 1;
+  #die("Argument blast_db must be supplied") unless $$settings{blast_db};
+  $$settings{query_mfa} = $ARGV[0];
+  die("File $$settings{query_mfa} does not exist") unless -f $$settings{query_mfa};
+  $$settings{outfile} ||= "$$settings{query_mfa}.iprscan_out.xml";
 
-my $invoke_string = "iprscan -cli -i '$$settings{query_mfa}' -o '$$settings{outfile}' -iprlookup -goterms";
-$invoke_string .= " -altjobs";
-logmsg "Running \"$invoke_string\"...";
-system($invoke_string);
-die if $?;
+  my $updateQueue=Thread::Queue->new;
+  my $updateThr=threads->create(\&monitorOutfile,$$settings{outfile},$updateQueue,$settings);
 
-logmsg "Parsing iprscan results in $$settings{outfile}...\n";
-my $invoke_string = "run_annotation_interpro_parse_xml.pl '$$settings{outfile}'";
-system($invoke_string);
-die if $?;
+  my $invoke_string = "iprscan -cli -i '$$settings{query_mfa}' -o '$$settings{outfile}' -iprlookup -goterms";
+  $invoke_string .= " -altjobs";
+  logmsg "Running \"$invoke_string\"...";
+  system($invoke_string);
+  die if $?;
+
+  logmsg "Parsing iprscan results in $$settings{outfile}...\n";
+  my $invoke_string = "run_annotation_interpro_parse_xml.pl '$$settings{outfile}'";
+  system($invoke_string);
+  die if $?;
+
+  # send a term signal to the queue
+  $updateQueue->enqueue(undef);
+  $updateThr->join;
+  return 0;
+}
+
+sub monitorOutfile{
+  my($outfile,$Q,$settings)=@_;
+  sleep 5 while(!-e $outfile);
+  while($Q->pending<1){
+    my $numFinished=`grep -c "<protein" '$outfile'`+0;
+    logmsg "Scanned approximately $numFinished proteins so far";
+    for(1..30){
+      last if($Q->pending>0);
+      sleep 2;
+    }
+  }
+  logmsg "Finished with interproscan";
+  return 1;
+}
