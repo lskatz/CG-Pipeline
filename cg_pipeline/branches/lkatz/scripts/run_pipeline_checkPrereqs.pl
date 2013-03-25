f3e678a75a12b6f33a35405f5cfb631ed2cc3167
lskatz <lskatz@347234af-12d4-46d6-bdbb-00f489ae4c7f>
2013-03-25 20:41:50 +0000

    added velvet to the prereq list

--- cg_pipeline/branches/lkatz/scripts/run_pipeline_checkPrereqs.pl
+++ cg_pipeline/branches/lkatz/scripts/run_pipeline_checkPrereqs.pl
@@ -21,7 +21,7 @@ use File::Basename;
 my $settings={
   appname=>'cgpipeline',
   perllibs=>[qw(AKUtils BerkeleyDB Bio::Assembly::IO Bio::Assembly::Scaffold Bio::Perl Bio::Seq Bio::SeqIO Bio::Seq::Quality Bio::Seq::RichSeq Bio::SeqUtils Bio::Tools::GFF Bio::Tools::Run::StandAloneBlast CGPBase CGPipeline::SQLiteDB CGPipelineUtils Data::Dumper Date::Format File::Basename File::Copy File::Path File::Spec File::Temp FindBin Getopt::Long GTTmhmm List::Util LWP::Simple Math::Round strict Thread::Queue threads threads::shared warnings XML::LibXML::Reader XML::Quote)],
-  executables=>[qw(addRun amos2ace AMOScmp cat cp gzip gunzip ln minimus2 mkdir nesoni newAssembly newMapping rm runProject setRef sfffile toAmos toAmos_new touch tmhmm signalp bam2fastq vcfutils.pl bcftools fastqqc)],
+  executables=>[qw(addRun amos2ace AMOScmp cat cp gzip gunzip ln minimus2 mkdir nesoni newAssembly newMapping rm runProject setRef sfffile toAmos toAmos_new touch tmhmm signalp bam2fastq vcfutils.pl bcftools fastqqc velveth velvetg VelvetOptimiser.pl)],
   environmentalVariables=>[qw(TMHMMDIR)],
 
   # presence/absence codes
