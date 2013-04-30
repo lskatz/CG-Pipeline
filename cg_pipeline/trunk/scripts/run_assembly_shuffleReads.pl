8dd2a5900d1eb6a775d912b2d875ad12fe8629e6
lskatz <lskatz@347234af-12d4-46d6-bdbb-00f489ae4c7f>
2013-04-30 20:30:37 +0000

    trying to move my branch to the trunk

---
+++ cg_pipeline/trunk/scripts/run_assembly_shuffleReads.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/env perl
+# shuffle or deshuffle sequences.  Good for fastq files only right now.
+# Author: Lee Katz <lkatz@cdc.gov>
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Basename;
+
+
+local $SIG{'__DIE__'} = sub { my $e = $_[0]; $e =~ s/(at [^\s]+? line \d+\.$)/\nStopped $1/; die("$0: ".(caller(1))[3].": ".$e); };
+exit(main());
+
+sub main{
+  my $settings={};
+  GetOptions($settings,qw(deshuffle help));
+  die usage() if($$settings{help});
+
+  my $numReads;
+  if($$settings{deshuffle}){
+    die "ERROR: need a file to deshuffle\n". usage() if(@ARGV<1);
+    my($seqFile)=@ARGV;
+    $numReads=deshuffleSeqs($seqFile,$settings);
+  } else {
+    die "ERROR: need >two files to shuffle\n". usage() if(@ARGV<2);
+    die "ERROR: need an even number of files to shuffle\n".usage() if(@ARGV % 2==1);
+    my @seqFile=@ARGV;
+    $numReads=shuffleSeqs(\@seqFile,$settings);
+  }
+  return 0;
+}
+
+sub deshuffleSeqs{
+  my($seqFile,$settings)=@_;
+  open(SHUFFLED,"<",$seqFile) or die "Could not open shuffled fastq file $seqFile: $!";
+  my $i=0;
+  while(<SHUFFLED>){
+    my $mod=$i%8;
+    print STDOUT $_ if($mod<4);
+    print STDERR $_ if($mod>=4);
+    
+    $i++;
+  }
+  close SHUFFLED;
+  my $numReads=$i/4;
+  return $numReads;
+}
+
+sub shuffleSeqs{
+  my($seqFile,$settings)=@_;
+  my $i=0;
+  for(my $j=0;$j<@$seqFile;$j+=2){
+    my($file1,$file2)=($$seqFile[$j],$$seqFile[$j+1]);
+    open(MATE1,"<",$file1) or die "Could not open $file1: $!";
+    open(MATE2,"<",$file2) or die "Could not open $file2: $!";
+    while(my $out=<MATE1>){
+      $out.=<MATE1> for(1..3);
+      $out.=<MATE2> for(1..4);
+      print STDOUT $out;
+      $i++;
+    }
+    close MATE1; close MATE2;
+  }
+  my $numReads=$i/4;
+  return $numReads;
+}
+
+sub usage{
+  local $0=fileparse($0);
+  "Shuffle or deshuffle sequences
+  Usage: $0 -d shuffled.fastq > file_1.fastq 2> file_2.fastq
+  $0 file_1.fastq file_2.fastq > shuffled.fastq
+  $0 file_[12].fastq > shuffled.fastq
+    -d for deshuffle
+  Due to the double redirection, error messages are hidden. A user should check the exit code to see if the program executed correctly.
+  "
+}
---
+++ cg_pipeline/trunk/scripts/run_pipeline_manage.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/env perl
+
+# run_pipeline_manage: manage a CGP project directory
+# Author: Lee Katz <lkatz@cdc.gov>
+
+package PipelineRunner;
+my ($VERSION) = ('$Id: $' =~ /,v\s+(\d+\S+)/o);
+
+my $settings = {
+  appname => 'cgpipeline',
+};
+
+use strict;
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+$ENV{PATH} = "$FindBin::RealBin:".$ENV{PATH};
+use AKUtils;
+
+use Getopt::Long;
+use File::Temp ('tempdir');
+use File::Path;
+use File::Spec;
+use File::Copy;
+use File::Basename;
+use List::Util qw(min max sum shuffle);
+use CGPipelineUtils;
+use Data::Dumper;
+use Archive::Tar;
+use File::Slurp;
+
+$0=fileparse $0;
+sub logmsg {my $FH = *STDERR; print $FH "$0: ".(caller(1))[3].": @_\n";}
+exit(main());
+
+sub main{
+  die usage() if @ARGV<1;
+  GetOptions($settings,qw(export clean));
+  die usage() if @ARGV<1;
+
+  my @project=@ARGV;
+
+  for my $p(@project){
+    if(!is_cgp_project($p,$settings)){
+      logmsg "$p is not a CGP project. I will skip it.";
+      next;
+    } else { logmsg "CGP project $p"; }
+    exportProject($p,$settings) if($$settings{export});
+    cleanProject($p,$settings) if($$settings{clean});
+  }
+  return 0;
+}
+
+sub is_cgp_project{
+  my($p,$settings)=@_;
+  for("$p","$p/annotation","$p/log","$p/build","$p/build/assembly","$p/build/prediction","$p/build/annotation"){
+    return 0 if(!-d $_);
+  }
+  return 1;
+}
+
+sub cleanProject{
+  my($project,$settings)=@_;
+  for(glob("$project/build/assembly/* $project/build/prediction/* $project/build/annotation/*")){
+    File::Path->remove_tree($_,{keep_root=>1}) or die "Could not remove $_: $!";
+  }
+  return 1;
+}
+  
+sub exportProject{
+  my($project,$settings)=@_;
+  my $file=locateFile($project,$settings);
+  my $fileContents=read_file($file);
+  print $fileContents;
+  return length($fileContents);
+}
+
+# locates the most advanced file: assembly, prediction, or annotation
+sub locateFile{
+  my($project,$settings)=@_;
+  my @desiredFile=qw(annotation.gb prediction.gb assembly.fasta);
+  for(@desiredFile){
+    my $file="$project/$_";
+    if(-e $file && -s $file>0){
+      return $file;
+    }
+  }
+  die "Could not locate a genome project file in $project out of ".join(" ",@desiredFile);
+}
+
+sub usage{
+  "Manages a CG-Pipeline project. With no options, simply reports whether a directory is a CGP project.
+  Usage: $0 project [project2 ...] -e > outfile
+  project: a cg_pipeline project directory (created by 'run_pipeline create')
+  -e export the project directory
+  -c clean the project directory
+  ";
+}
