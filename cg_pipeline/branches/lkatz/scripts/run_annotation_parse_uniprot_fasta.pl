e1f4bbdd1bbc2f08a4802e18a4c0e13ad08de8e8
Lee Katz <lskatz@users.noreply.github.com>
2014-05-28 18:17:46 +0000

    added EC numbers and an option to keep entries with no gene names

--- cg_pipeline/branches/lkatz/scripts/run_annotation_parse_uniprot_fasta.pl
+++ cg_pipeline/branches/lkatz/scripts/run_annotation_parse_uniprot_fasta.pl
@@ -16,17 +16,20 @@ use AKUtils;
 sub logmsg{print STDERR "@_\n";}
 exit main();
 sub main{
-  my $settings={};
-  GetOptions($settings,qw(help tempdir=s numcpus=i)) or die;
+  my $settings={
+    cluster=>1,
+  };
+  GetOptions($settings,qw(help tempdir=s numcpus=i cluster! need-genes)) or die;
   die usage() if($$settings{help});
   die "ERROR: need fasta files\n".usage() if(!@ARGV);
   $$settings{tempdir}||=AKUtils::mktempdir();
   $$settings{numcpus}||=1;
   $$settings{maxmem}||=int(AKUtils::getFreeMem() * 0.75+ 1); # 75% of free memory plus one byte to avoid zero
+  $$settings{'need-genes'}||=0;
   my @fasta=@ARGV;
   my @genera=getGenera($settings);
   my $filtered=filterByGenusAndReformatDefline(\@fasta,\@genera,$settings);
-  my $clustered=clusterSequences($filtered,$settings);
+  my $clustered=clusterSequences($filtered,$settings) if($$settings{cluster});
   system("cat '$clustered'"); die if $?;
   return 0;
 }
@@ -62,7 +65,7 @@ sub filterByGenusAndReformatDefline{
     if($suffix eq '.gz'){
       $in=Bio::SeqIO->new(-format=>"fasta",-file=>"gunzip -c '$fastain' | ");
     }else{
-      $in=Bio::SeqIO->new(-file=>$fastain);
+      $in=Bio::SeqIO->new(-file=>$fastain,-format=>"fasta");
     }
     my $i=0;
     my $writtenCounter=0;
@@ -73,14 +76,24 @@ sub filterByGenusAndReformatDefline{
       }
       my $id=$seq->id." ".$seq->desc;
       next if($id !~ /$genusRegex/);
-      my($seqid,$product,$gene)=("","","");
+      my($seqid,$product,$gene,$EC)=("","","","");
       if($id=~/^\s*(\S+)\s+(.*?)\s*\w+=/){
         ($seqid,$product)=($1,$2);
       }
-      next if($id !~ /GN=(\S+)/);
-      $gene=$1;
+      if($id =~ /GN=(\S+)/){
+        $gene=$1;
+      } else {
+        next if($$settings{'need-genes'});
+      }
+
+      # (EC 3.4.19.1)
+      # EC numbers can end with a dash to show that the catalytic activity of the protein is not known exactly.
+      # EC numbers can end with an n and a number to show that it catalyzes a reaction that is known but not yet included in the IUBMB EC list.
+      if($id=~/\(EC (\d+(\.n?[\d\-]+){3})\)/){
+        $EC=$1;
+      }
       $seq->id($seqid);
-      $seq->desc("~~~$gene~~~$product");
+      $seq->desc("$EC~~~$gene~~~$product");
       $out->write_seq($seq);
       $writtenCounter++;
     }
@@ -101,9 +114,11 @@ sub clusterSequences{
 
 sub usage{
   local $0=fileparse $0;
-  "Filters a uniprot-style fasta file with the genera in the bacterial kingdom. Entries with no gene name or no product name are also filtered out. Then, filtered genes are clustered with cd-hit to reduce the size of the database.
+  "Filters a uniprot-style fasta file with the genera in the bacterial kingdom. Entries with no gene name are also filtered out if --need-genes is specified. Then, filtered genes are clustered with cd-hit to reduce the size of the database.
   Usage: $0 uniprot.fasta[.gz] [uniprot2.fasta[.gz] ...] > filtered.fasta
-  -n 1 number of cpus
+  --numcpus 1   Number of cpus
+  --need-genes  To skip any entries without a gene name
+  --nocluster   Do not cluster sequences with cd-hit
   -t tempdir/
   "
 }
