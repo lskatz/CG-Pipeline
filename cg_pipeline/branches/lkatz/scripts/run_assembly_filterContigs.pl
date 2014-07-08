0a117c41af65ebe3b726ff932bc9778ca8562c0d
lskatz <lskatz@347234af-12d4-46d6-bdbb-00f489ae4c7f>
2014-07-08 19:21:33 +0000

    a script to filter out bad contigs

---
+++ cg_pipeline/branches/lkatz/scripts/run_assembly_filterContigs.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/env perl
+
+# run-assembly-filterContigs: remove contigs that fit certain criteria
+# Author: Lee Katz (lkatz@cdc.gov)
+
+package PipelineRunner;
+my ($VERSION) = ('$Id: $' =~ /,v\s+(\d+\S+)/o);
+
+my $settings = {
+	appname => 'cgpipeline',
+};
+my $stats;
+
+use strict;
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+$ENV{PATH} = "$FindBin::RealBin:".$ENV{PATH};
+use AKUtils qw(logmsg);
+
+use Getopt::Long;
+use File::Temp ('tempdir');
+use File::Path;
+use File::Spec;
+use File::Basename;
+use Data::Dumper;
+
+$0 = fileparse($0);
+local $SIG{'__DIE__'} = sub { my $e = $_[0]; $e =~ s/(at [^\s]+? line \d+\.$)/\nStopped $1/; die("$0: ".(caller(1))[3].": ".$e); };
+sub logmsg {my $FH = $FSFind::LOG || *STDERR; print $FH "$0: ".(caller(1))[3].": @_\n";}
+
+exit(main());
+
+sub main() {
+	$settings = AKUtils::loadConfig($settings);
+
+	my @cmd_options = qw(length=i help numcpus=i);
+	GetOptions($settings, @cmd_options) or die;
+	$$settings{numcpus}||=1;
+  $$settings{assembly_min_contig_length}||=500;
+	die usage($settings) if @ARGV < 1;
+	die usage($settings) if ($$settings{help});
+
+  my %seq;
+  for(@ARGV){
+    my $seq=AKUtils::readMfa($_,$settings);
+    $seq=filterSeqs($seq,$settings);
+    %seq=(%seq,%$seq);
+  }
+
+  while(my($id,$sequence)=each(%seq)){
+    $sequence=~s/(.{80})/$1\n/g;
+    print ">$id\n$sequence\n";
+  }
+
+  return 0;
+}
+
+sub filterSeqs{
+  my($seq,$settings)=@_;
+  my %seq;
+  while(my($id,$sequence)=each(%$seq)){
+    $seq{$id}=$sequence if(length($sequence) > $$settings{assembly_min_contig_length});
+  }
+  return \%seq;
+}
+
+sub usage{
+  my ($settings)=@_;
+  "Filters sequences from a fasta file that do not meet certain criteria and prints them to stdout
+  Usage: $0 file.fasta [file2.fasta...] > out.fasta
+  -l $$settings{assembly_min_contig_length} Minimum size of a contig
+  "
+}
