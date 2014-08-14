367afb0c0203b1143b740150a122a199844fa593
lskatz <lskatz@347234af-12d4-46d6-bdbb-00f489ae4c7f>
2014-08-14 19:42:24 +0000

    more stable. Better checking for EC numbers. Getting xrefs

--- cg_pipeline/branches/lkatz/scripts/run_annotation_parse_uniprot_fasta.pl
+++ cg_pipeline/branches/lkatz/scripts/run_annotation_parse_uniprot_fasta.pl
@@ -22,11 +22,15 @@ sub main{
   GetOptions($settings,qw(help tempdir=s numcpus=i cluster! need-genes)) or die;
   die usage() if($$settings{help});
   die "ERROR: need fasta files\n".usage() if(!@ARGV);
-  $$settings{tempdir}||=AKUtils::mktempdir();
+  $$settings{tempdir}||=AKUtils::mktempdir(); mkdir($$settings{tempdir}) if(!-d $$settings{tempdir});
   $$settings{numcpus}||=1;
   $$settings{maxmem}||=int(AKUtils::getFreeMem() * 0.75+ 1); # 75% of free memory plus one byte to avoid zero
   $$settings{'need-genes'}||=0;
   my @fasta=@ARGV;
+  for(@fasta){
+    die "ERROR: I could not find fasta file $_" if(!-f $_);
+  }
+
   my @genera=getGenera($settings);
   my $filtered=filterByGenusAndReformatDefline(\@fasta,\@genera,$settings);
   my $clustered=clusterSequences($filtered,$settings) if($$settings{cluster});
@@ -72,39 +76,48 @@ sub filterByGenusAndReformatDefline{
     my $writtenCounter=0;
     while(my $seq=$in->next_seq){
       if(++$i % 100000 == 0){
-        $out->write_seq(@seq);
-        $writtenCounter+=@seq;
-        @seq=();
+        $out->write_seq(@seqBuffer);
+        $writtenCounter+=@seqBuffer;
+        @seqBuffer=();
 
         my $percent=sprintf("%0.2f",$writtenCounter/$i*100);
-        logmsg "$percent% written: ". $i." entries read. $writtenCounter entries written.";
+        logmsg "$percent% written out of the current $i entries ($writtenCounter).";
       }
       my $id=$seq->id." ".$seq->desc;
       next if($id !~ /$genusRegex/);
-      my($seqid,$product,$gene,$EC)=("","","","");
-      if($id=~/^\s*(\S+)\s+(.*?)\s*\w+=/){
+      my($seqid,$product,$gene,$EC,$xref)=("","","","","");
+      # Get the sequence identifier and the product
+      if($id=~/^\s*(\S+)\s+(.*?)\s*\w+=/){ # regex: first nonwhitespace; next nonwhitespace until any key/value pair with an equals sign
         ($seqid,$product)=($1,$2);
       }
+      # Get the gene name
       if($id =~ /GN=(\S+)/){
         $gene=$1;
       } else {
+        # Ignore this gene entirely if there is no gene name and you want them
         next if($$settings{'need-genes'});
+      }
+
+      # get the xref if possible
+      # e.g. sp|P0A5B8|14KD_MYCBO => 14KD_MYCBO
+      if($id=~/(\w+)\|(\w+)(\|(\w+))?/){
+        $xref=$4;
       }
 
       # (EC 3.4.19.1)
       # EC numbers can end with a dash to show that the catalytic activity of the protein is not known exactly.
       # EC numbers can end with an n and a number to show that it catalyzes a reaction that is known but not yet included in the IUBMB EC list.
-      if($id=~/\(EC (\d+(\.n?[\d\-]+){3})\)/){
+      if($id=~/(\d+(\.\d+){3})/){
         $EC=$1;
       }
       $seq->id($seqid);
-      $seq->desc("$EC~~~$gene~~~$product");
+      $seq->desc(join("~~~",$xref,$EC,$gene,$product));
       push(@seqBuffer,$seq);
     }
     # finish writing the rest to file
-    $out->write_seq(@seq);
-    $writtenCounter+=@seq;
-    @seq=();
+    $out->write_seq(@seqBuffer);
+    $writtenCounter+=@seqBuffer;
+    @seqBuffer=();
   }
   system ("mv -v '$tmpfile' '$outfile' 1>&2"); die if $?;
   return $outfile;
@@ -123,6 +136,7 @@ sub clusterSequences{
 sub usage{
   local $0=fileparse $0;
   "Filters a uniprot-style fasta file with the genera in the bacterial kingdom. Entries with no gene name are also filtered out if --need-genes is specified. Then, filtered genes are clustered with cd-hit to reduce the size of the database.
+  Deflines will be in the format of ~~~
   Usage: $0 uniprot.fasta[.gz] [uniprot2.fasta[.gz] ...] > filtered.fasta
   --numcpus 1   Number of cpus
   --need-genes  To skip any entries without a gene name
