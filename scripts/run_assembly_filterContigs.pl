be9ec3752c14a4413d1f8c3ae688ea2ce2b10f0b
Lee Katz - Aspen <gzu2@cdc.gov>
2024-10-09 10:47:29 -0400

    added coverage filter

--- scripts/run_assembly_filterContigs.pl
+++ scripts/run_assembly_filterContigs.pl
@@ -33,10 +33,11 @@ exit(main());
 sub main() {
 	$settings = AKUtils::loadConfig($settings);
 
-	my @cmd_options = qw(verbose assembly_min_contig_length|length=i help numcpus=i rename);
+	my @cmd_options = qw(verbose cov|min_cov=i assembly_min_contig_length|length=i help numcpus=i rename);
 	GetOptions($settings, @cmd_options) or die;
 	$$settings{numcpus}||=1;
   $$settings{assembly_min_contig_length}||=500;
+  $$settings{cov} ||= 0;
 	die usage($settings) if @ARGV < 1;
 	die usage($settings) if ($$settings{help});
 
@@ -61,8 +62,20 @@ sub filterSeqs{
   my %seq;
   my $i=0;
   while(my($id,$sequence)=each(%$seq)){
+    # Remove short contigs
     if(length($sequence) < $$settings{assembly_min_contig_length}){
       logmsg "Removing short contig $id" if($$settings{verbose});
+      next;
+    }
+
+    # Parse the ID for coverage: usually in the format of cov_\d+\.\d+
+    # Remove the contig if the coverage is too low.
+    my $cov;
+    if($id=~/cov_(\d+(\.\d+)?)/){
+      $cov=$1;
+    }
+    if(defined $cov && $cov < $$settings{cov}){
+      logmsg "Removing low coverage contig $id with coverage $cov" if($$settings{verbose});
       next;
     }
     
@@ -71,7 +84,9 @@ sub filterSeqs{
     my $uncomplexkmer="";
     for(my $k=1;$k<20;$k++){
       my $kmer=substr($sequence,0,$k);
-      if($sequence=~/^($kmer)+$/i){
+      # If we see a repeat of the kmer from start to finish, then it's uncomplex.
+      # There is a {0,$k} to account for a truncated kmer at the end of the sequence.
+      if($sequence=~/^($kmer)+(.{0,$k})$/i){
         $uncomplexkmer=$kmer;
         last;
       }
@@ -97,6 +112,7 @@ sub usage{
   "Filters sequences from a fasta file that do not meet certain criteria and prints them to stdout
   Usage: $0 file.fasta [file2.fasta...] > out.fasta
   --length  $$settings{assembly_min_contig_length}   Minimum size of a contig
+  --cov     $$settings{cov}                          Minimum coverage of a contig - detected by searching deflines for cov_f where f is a floating point number
   --rename        rename contigs
   --verbose       
   "
